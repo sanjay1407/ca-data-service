2dcab47f40c6a0fd7a019741486820eb07e99997
Imran Iaskar <ilaskar@falabella.cl>
2019-11-08 01:01:53 +0530

    ffcfgghf

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import snoopdog from "./snoopdog";
+import sniffdog from "./sniffdog";
 
 const router = Router();
 router.get("/", (req, res) => res.send("Welcome to Frontend Social API"));
 router.use("/snoopdog", snoopdog);
+router.use("/khoj", sniffdog);
 
 export default router;
--- src/controller/snoopdog.js
+++ src/controller/snoopdog.js
@@ -6,8 +6,4 @@ router.get("/", (req, res) => {
   res.send(snoopUrls);
 });
 
-// router.get("/:id", (req, res) => {
-//   res.send(user);
-// });
-
 export default router;
